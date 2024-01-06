fccd786b6994827f16b8d6326ce97774137a48c0
김하연 <hayeonkim226@gmail.com>
2024-01-06 15:51:06 +0900

    fix: parallel routes 스타일 수정

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function DashboardPage() {
   return (
-    <div className="text-center bg-rose-700 text-white font-semibold flex flex-col justify-center">
+    <div className="text-center bg-rose-500 text-white font-semibold flex flex-col justify-center">
       Dashboard Main Page
     </div>
   )
